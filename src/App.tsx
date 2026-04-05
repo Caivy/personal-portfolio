ff21ced08d1df008f460c39afaff178308fb3ecd
Caivy <samnangkhath5@gmail.com>
2026-04-05 21:00:53 +0700

    feat: circle avatar is overlay ontop of banner images and responsiveness on mobile
    we slightly nudge the circle avatar a bit to the top

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/header";
 function App() {
   return (
     <main className="min-h-screen bg-black">
-      <div className="flex flex-col container mx-auto max-w-6xl">
+      <div className="flex flex-col mx-auto max-w-6xl">
         <Header />
       </div>
     </main>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,25 @@
 export default function Header() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="pt-4 flex flex-col gap-4 ">
       <img
-        src="https://placehold.co/1200x60"
-        width="1200"
-        height="60"
-        className="h-60 object-cover items-center"
+        src="https://placehold.co/750x200"
+        height="200"
+        className="max-w-full rounded-xl object-fill items-center"
       />
-      <div className="flex flex-row gap-8 items-start">
+      <div className="flex flex-col lg:flex-row items-center lg:items-start lg:gap-8 -translate-y-8 lg:translate-y-0">
         <img
           src="https://elouwerse.nl/placecircle/200"
-          className="rounded-full border-amber-600 border-4"
+          className="rounded-full border-amber-600 border-4 items-center -translate-y-8 lg:-translate-y-10"
         />
-        <div className="flex flex-col item-start">
+        <div className="flex flex-col items-center lg:items-start ">
           <h1 className="text-white font-bold text-2xl">Khath Samnang</h1>
-          <p className="pt-4 text-white font-normal text-xl whitespace-break-spaces">
-            Graphic Designer / Full-Stack Developer / IT Support
-            <br />
-            Has always been Passionate about computers and tinkering with
-            technologies.
+          <p className="pt-4 max-w-1xl text-center lg:text-start text-white font-normal text-lg">
+            <span className="block">
+              Graphic Designer / Full-Stack Developer / IT Support
+            </span>
+            <span className="block">
+              Passionate about computers and tinkering with technologies.
+            </span>
           </p>
           <button className="mt-4 bg-orange-500 w-38 h-12 rounded-sm text-center text-white font-bold text-xl">
             Contact Me

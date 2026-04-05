75d42f2608a42e6e0fb2f0eff7515c2c90426a8a
Caivy <samnangkhath5@gmail.com>
2026-04-05 21:14:19 +0700

    feat: add profile picture and banners images

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,20 @@
+import samnang from "../assets/samnang.jpg";
+import banner from "../assets/banner.jpg";
+
 export default function Header() {
   return (
-    <div className="pt-4 flex flex-col gap-4 ">
+    <div className="lg:pt-2 flex flex-col gap-4">
       <img
-        src="https://placehold.co/750x200"
+        src={banner}
         height="200"
-        className="max-w-full rounded-xl object-fill items-center"
+        className="max-w-full lg:h-80 rounded-b-xl lg:rounded-xl object-cover items-center"
       />
       <div className="flex flex-col lg:flex-row items-center lg:items-start lg:gap-8 -translate-y-8 lg:translate-y-0">
         <img
-          src="https://elouwerse.nl/placecircle/200"
+          src={samnang}
           className="rounded-full border-amber-600 border-4 items-center -translate-y-8 lg:-translate-y-10"
+          width="200"
+          height="200"
         />
         <div className="flex flex-col items-center lg:items-start ">
           <h1 className="text-white font-bold text-2xl">Khath Samnang</h1>
